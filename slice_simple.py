2b7342da677258dad77391f25c868774e3aa4516
Victoria De Santis <162721946+vicdsantis@users.noreply.github.com>
2024-04-05 12:32:07 -0300

    entrega slice_simple.py

--- slice_simple.py
+++ slice_simple.py
@@ -1,5 +1,10 @@
 def slice_simple():
     texto = "Awesome"
+    print(texto.lower() [0:3])
+    print(texto.lower() [2:5])
+    print(texto.lower()[:])
+
+
     # Código a implementar, se debe utilizar la variable 'texto' para resolver el ejercicio.
     # No se debe modificar la definición de la función, ni ingresar otro valor mediante input.
 
