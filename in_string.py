f17fd3975bd99cfe13c952d7d14d3ff961314c34
Victoria De Santis <162721946+vicdsantis@users.noreply.github.com>
2024-04-05 12:29:21 -0300

    entrega in_string.py

--- in_string.py
+++ in_string.py
@@ -1,4 +1,11 @@
 def check_vowels():
+    txt= input("")
+    print("Contiene a: " + str("a" in txt.lower()))
+    print("Contiene e: " + str("e" in txt.lower()))
+    print("Contiene i: " + str("i" in txt.lower()))
+    print("Contiene o: " + str("o" in txt.lower()))
+    print("Contiene u: " + str("u" in txt.lower()))
+    
     # Código a implementar utilizando input.
 
 
